d9e37909719956cb0e4f3d30d092588c88301d90
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2025-01-13 01:29:15 +0000

    A CIRCLE HAS MOVED

--- src/fireworks_gl.c
+++ src/fireworks_gl.c
@@ -301,7 +301,7 @@ void FWGL_prepareBuffers(struct FWGL* fwgl) {
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glVertexAttribDivisor(2, 1); // Stride of 1 between swapping attributes
+    glVertexAttribDivisor(1, 1); // Stride of 1 between swapping attributes
 
     glBindVertexArray(0);
 
